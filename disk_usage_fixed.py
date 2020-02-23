f3f9dd908ab3993ce1ae5925d96e6d551dd2540e
Hasibul-Alam <halambd7@gmail.com>
2020-02-23 11:45:02 +0600

    Two cp file and a diff file added

--- disk_usage_fixed.py
+++ disk_usage_fixed.py
@@ -1,16 +1,18 @@
 import shutil
+import sys
 
 def check_disk_usage(disk, min_absolute, min_percent):
     # return True if there is enough free disk space, False otherwise.
     du = shutil.disk_usage(disk)
     percent_free = 100 * du.free / du.total
     gigabyte_free = du.free / 2**30
-    if precent_free < min_percent or gigabyte_free < min_absolute:
+    if percent_free < min_percent or gigabyte_free < min_absolute:
         return False
     return True
 
-if not check_disk_usage('F:/',23,30):
+# if not check_disk_usage('F:/',23,30): @ if windows OS
+if not check_disk_usage('/', 2, 10):
     print ('Error: Not enough disk space')
-    return 1
+    sys.exit(1)
 print ('Everything ok!')
-return 0
+sys.exit(0)
